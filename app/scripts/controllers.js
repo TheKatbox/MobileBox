ec15de077685cd459bc21e50a818fdbc3d795a4a
Devin Leonhart <devinleonhart@gmail.com>
2016-01-19 21:29:16 -0800

    Added project scaffolding and placeholder views.

---
+++ app/scripts/controllers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('starter.controllers', [])
+
+.controller('ComicCtrl', function() {})
+
+.controller('StoreCtrl', function() {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+})
+
+.controller('ItemDetailCtrl', function() {
+})
+
+.controller('AccountCtrl', function() {
+});
